b8f3627fbc1f9a8ab0fa6d08b5493f314503799d
Borislav Ivanov <by.ivanov@gmail.com>
2016-02-25 18:16:05 +0200

    EXIF data from JSON to HTML table

--- main.js
+++ main.js
@@ -37,6 +37,57 @@ $(document).ready(function() {
           var orientation = EXIF.getTag(this, "Orientation");
           correctOrientation(orientation);
           //console.log(EXIF.pretty(this));
+          var obj = {};
+          var tags = ['Make', 'Model', 'DateTime', 'Artist', 'Copyright', 'ExposureTime ', 'FNumber', 'ISOSpeedRatings', 'ShutterSpeedValue', 'ApertureValue', 'FocalLength'];
+          for (var tag in tags) {
+            var prop = tags[tag];
+            if (this.exifdata.hasOwnProperty(prop)) {
+              obj[prop] = this.exifdata[prop];
+            }
+          }
+          jQuery.extend(obj, this.iptcdata);
+          $('#exifExtensionModal').on('shown.bs.modal', function () {
+          //  document.write("EXIF Object" + JSON.stringify(obj));
+          var myList = JSON.stringify(obj);
+    // Builds the HTML Table out of myList json data from Ivy restful service.
+ function buildHtmlTable() {
+     var columns = addAllColumnHeaders(myList);
+
+     for (var i = 0 ; i < myList.length ; i++) {
+         var row$ = $('<tr/>');
+         for (var colIndex = 0 ; colIndex < columns.length ; colIndex++) {
+             var cellValue = myList[i][columns[colIndex]];
+
+             if (cellValue == null) { cellValue = ""; }
+
+             row$.append($('<td/>').html(cellValue));
+         }
+         $("#excelDataTable").append(row$);
+     }
+ }
+
+ // Adds a header row to the table and returns the set of columns.
+ // Need to do union of keys from all records as some records may not contain
+ // all records
+ function addAllColumnHeaders(myList)
+ {
+     var columnSet = [];
+     var headerTr$ = $('<tr/>');
+
+     for (var i = 0 ; i < myList.length ; i++) {
+         var rowHash = myList[i];
+         for (var key in rowHash) {
+             if ($.inArray(key, columnSet) == -1){
+                 columnSet.push(key);
+                 headerTr$.append($('<th/>').html(key));
+             }
+         }
+     }
+     $("#excelDataTable").append(headerTr$);
+
+     return columnSet;
+ }
+          });
         });
       }
     });
