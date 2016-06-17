82cd2ded88ec24c8e95bda92717aeaad1e2a61dc
kristiyandd <kristiyandd@gmail.com>
2016-06-17 17:58:50 +0300

    common extension with js/css files

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /* Copyright (c) 2013-2016 The TagSpaces Authors.
  * Use of this source code is governed by the MIT license which can be found in the LICENSE.txt file. */
 
-/* globals Nanobar, marked, EXIF */
+/* globals Nanobar, marked, EXIF, Mousetrap  */
 "use strict";
 
 $(document).ready(function() {
@@ -24,42 +24,9 @@ $(document).ready(function() {
     filePath = "file://" + filePath;
   }
 
-  $(document).on('drop dragend dragenter dragover' , function(event) {
-    event.preventDefault();
-  });
 
-  var $imgViewer = $("#imgViewer");
+  var $imgViewer = $("#htmlContent");
   var exifObj;
-
-
-  $('#aboutExtensionModal').on('show.bs.modal' , function() {
-    $.ajax({
-      url: 'README.md' ,
-      type: 'GET'
-    }).done(function(mdData) {
-      //console.log("DATA: " + mdData);
-      if (marked) {
-        var modalBody = $("#aboutExtensionModal .modal-body");
-        modalBody.html(marked(mdData , {sanitize: true}));
-        handleLinks(modalBody);
-      } else {
-        console.log("markdown to html transformer not found");
-      }
-    }).fail(function(data) {
-      console.warn("Loading file failed " + data);
-    });
-  });
-
-  function handleLinks($element) {
-    $element.find("a[href]").each(function() {
-      var currentSrc = $(this).attr("href");
-      $(this).bind('click' , function(e) {
-        e.preventDefault();
-        var msg = {command: "openLinkExternally" , link: currentSrc};
-        window.parent.postMessage(JSON.stringify(msg) , "*");
-      });
-    });
-  }
 
   $("#imageContent").attr("src" , filePath).bind("load" , function() {
     $(this).addClass("transparentImageBackground");
@@ -188,14 +155,6 @@ $(document).ready(function() {
         imageRotationClass = "";
     }
     $image.addClass(imageRotationClass);
-  });
-
-  $("#aboutButton").on("click", function(e) {
-    $("#aboutExtensionModal").modal({show: true});
-  });
-
-  $("#printButton").on("click", function(e) {
-    window.print();
   });
 
   if (isCordova) {
