5fb6b994c94887e1aad3baa0ea7ea0c228d1344c
kristiyandd <kristiyandd@gmail.com>
2016-10-18 10:52:18 +0300

    update

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 /* Copyright (c) 2013-2016 The TagSpaces Authors.
  * Use of this source code is governed by the MIT license which can be found in the LICENSE.txt file. */
 
-/* globals Nanobar, marked, EXIF, Mousetrap  */
+/* globals Nanobar, marked, EXIF, Mousetrap, Viewer  */
 "use strict";
 
 $(document).ready(function() {
   function getParameterByName(name) {
-    name = name.replace(/[\[]/ , "\\\[").replace(/[\]]/ , "\\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)") ,
-            results = regex.exec(location.search);
-    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g , " "));
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+      results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
   }
 
   var filePath = getParameterByName("cp");
@@ -28,31 +28,62 @@ $(document).ready(function() {
   var $imgViewer = $("#htmlContent");
   var exifObj;
 
-  $("#imageContent").attr("src" , filePath).bind("load" , function() {
-    $(this).addClass("transparentImageBackground");
-    $imgViewer.addClass("imgViewer");
-    if (filePath.toLowerCase().indexOf("jpg") === (filePath.length - 3) ||
-            filePath.toLowerCase().indexOf("jpeg") === (filePath.length - 4)) {
-      EXIF.getData(this , function() {
-        var orientation = EXIF.getTag(this , "Orientation");
-        correctOrientation(orientation);
-        //console.log(EXIF.pretty(this));
-        exifObj = {};
-        var tags = ['Make' , 'Model' , 'DateTime' , 'Artist' , 'Copyright' , 'ExposureTime ' , 'FNumber' , 'ISOSpeedRatings' , 'ShutterSpeedValue' , 'ApertureValue' , 'FocalLength'];
-        for (var tag in tags) {
-          var prop = tags[tag];
-          if (this.exifdata.hasOwnProperty(prop)) {
-            exifObj[prop] = this.exifdata[prop];
-          }
-        }
-        jQuery.extend(exifObj , this.iptcdata);
-        if (!jQuery.isEmptyObject(exifObj)) {
-          $("#exifButton").parent().show();
-          printEXIF();
-        }
-      });
+
+  var opt = {
+    fading: true,
+    // inline: true,
+    url: filePath,
+    ready: function(e) {
+      console.log(e.type);
+    },
+    show: function(e) {
+      console.log(e.type);
+    },
+    shown: function(e) {
+      console.log(e.type);
+    },
+    hide: function(e) {
+      console.log(e.type);
+    },
+    hidden: function(e) {
+      console.log(e.type);
+    },
+    view: function(e) {
+      console.log(e.type);
+    },
+    viewed: function(e) {
+      console.log(e.type);
+      // this.viewer.zoomTo(1).rotateTo(180);
     }
-  });
+  };
+  var viewer = new Viewer(document.getElementById('imageContent'), opt);
+  console.debug(viewer);
+  $("#imageContent").attr("src" , filePath);
+  //$("#imageContent").attr("src" , filePath).bind("load" , function() {
+  //  $(this).addClass("transparentImageBackground");
+  //  $imgViewer.addClass("imgViewer");
+  //  if (filePath.toLowerCase().indexOf("jpg") === (filePath.length - 3) ||
+  //          filePath.toLowerCase().indexOf("jpeg") === (filePath.length - 4)) {
+  //    EXIF.getData(this , function() {
+  //      var orientation = EXIF.getTag(this , "Orientation");
+  //      correctOrientation(orientation);
+  //      //console.log(EXIF.pretty(this));
+  //      exifObj = {};
+  //      var tags = ['Make' , 'Model' , 'DateTime' , 'Artist' , 'Copyright' , 'ExposureTime ' , 'FNumber' , 'ISOSpeedRatings' , 'ShutterSpeedValue' , 'ApertureValue' , 'FocalLength'];
+  //      for (var tag in tags) {
+  //        var prop = tags[tag];
+  //        if (this.exifdata.hasOwnProperty(prop)) {
+  //          exifObj[prop] = this.exifdata[prop];
+  //        }
+  //      }
+  //      jQuery.extend(exifObj , this.iptcdata);
+  //      if (!jQuery.isEmptyObject(exifObj)) {
+  //        $("#exifButton").parent().show();
+  //        printEXIF();
+  //      }
+  //    });
+  //  }
+  //});
 
   function printEXIF() {
     var $exifRow = $("#exifRow").clone(); // Preparing the template
@@ -68,25 +99,25 @@ $(document).ready(function() {
     }
   }
 
-  $imgViewer.panzoom({
-    $zoomIn: $("#zoomInButton") ,
-    $zoomOut: $("#zoomOutButton") ,
-    $reset: $("#zoomResetButton") ,
-    minScale: 0.1 ,
-    maxScale: 10 ,
-    increment: 0.2 ,
-    easing: "ease-in-out" ,
-    contain: 'invert'
-  }).parent().on('mousewheel.focal' , function(e) {
-    e.preventDefault();
-    var delta = e.delta || e.originalEvent.wheelDelta;
-    var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
-    $imgViewer.panzoom('zoom' , zoomOut , {
-      increment: 0.1 ,
-      focal: e ,
-      animate: false
-    });
-  });
+  //$imgViewer.panzoom({
+  //  $zoomIn: $("#zoomInButton") ,
+  //  $zoomOut: $("#zoomOutButton") ,
+  //  $reset: $("#zoomResetButton") ,
+  //  minScale: 0.1 ,
+  //  maxScale: 10 ,
+  //  increment: 0.2 ,
+  //  easing: "ease-in-out" ,
+  //  contain: 'invert'
+  //}).parent().on('mousewheel.focal' , function(e) {
+  //  e.preventDefault();
+  //  var delta = e.delta || e.originalEvent.wheelDelta;
+  //  var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
+  //  $imgViewer.panzoom('zoom' , zoomOut , {
+  //    increment: 0.1 ,
+  //    focal: e ,
+  //    animate: false
+  //  });
+  //});
 
   function correctOrientation(orientation) {
     var $image = $("#imageContent");
@@ -111,7 +142,7 @@ $(document).ready(function() {
     $image.addClass(imageRotationClass);
   }
 
-  $("#rotateLeftButton").on("click" , function() {
+  $("#rotateLeftButton").on("click", function() {
     //console.log("Rotate Left");
     var $image = $("#imageContent");
     $image.removeClass(imageRotationClass);
@@ -134,7 +165,7 @@ $(document).ready(function() {
     $image.addClass(imageRotationClass);
   });
 
-  $("#rotateRightButton").on("click" , function() {
+  $("#rotateRightButton").on("click", function() {
     //console.log("Rotate Right");
     var $image = $("#imageContent");
     $image.removeClass(imageRotationClass);
@@ -167,19 +198,19 @@ $(document).ready(function() {
   $.i18n.init({
     ns: {
       namespaces: ['ns.viewerImage']
-    } ,
-    debug: true ,
+    },
+    debug: true,
     fallbackLng: 'en_US'
-  } , function() {
+  }, function() {
     $('[data-i18n]').i18n();
   });
 
   // Nano progressbar
   $(function() {
     var options = {
-      bg: '#42BEDB' , // (optional) background css property, '#000' by default
+      bg: '#42BEDB', // (optional) background css property, '#000' by default
       // leave target blank for global nanobar
-      target: document.getElementById('nanoBar') , //(optional) Where to put the progress bar, nanobar will be fixed to top of document if target is null
+      target: document.getElementById('nanoBar'), //(optional) Where to put the progress bar, nanobar will be fixed to top of document if target is null
       // id for new nanobar
       id: 'nanoBar' // (optional) id for nanobar div
     };
